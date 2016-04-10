614a9831d25508c3abd42cc1cd6dbf634cab2215
Lefteris Paraskevas <elparask@gmail.com>
2016-04-10 21:17:52 +0100

    install angulartics via jspm

--- config.js
+++ config.js
@@ -29,7 +29,7 @@ System.config({
 
   map: {
     "angular2": "node_modules/angular2",
-    "angulartics2": "node_modules/angulartics2",
+    "angulartics2": "npm:angulartics2@1.0.1",
     "redux": "node_modules/redux/dist/redux.js",
     "redux-logger": "node_modules/redux-logger/dist/index.js",
     "rxjs": "node_modules/rxjs",
@@ -38,14 +38,40 @@ System.config({
     "github:frankwallis/plugin-typescript@4.0.2": {
       "typescript": "npm:typescript@1.8.9"
     },
+    "github:jspm/nodelibs-assert@0.1.0": {
+      "assert": "npm:assert@1.3.0"
+    },
     "github:jspm/nodelibs-os@0.1.0": {
       "os-browserify": "npm:os-browserify@0.1.2"
+    },
+    "github:jspm/nodelibs-process@0.1.2": {
+      "process": "npm:process@0.11.2"
+    },
+    "github:jspm/nodelibs-util@0.1.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:angulartics2@1.0.1": {
+      "process": "github:jspm/nodelibs-process@0.1.2",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    },
+    "npm:assert@1.3.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:inherits@2.0.1": {
+      "util": "github:jspm/nodelibs-util@0.1.0"
     },
     "npm:os-browserify@0.1.2": {
       "os": "github:jspm/nodelibs-os@0.1.0"
     },
+    "npm:process@0.11.2": {
+      "assert": "github:jspm/nodelibs-assert@0.1.0"
+    },
     "npm:typescript@1.8.9": {
       "os": "github:jspm/nodelibs-os@0.1.0"
+    },
+    "npm:util@0.10.3": {
+      "inherits": "npm:inherits@2.0.1",
+      "process": "github:jspm/nodelibs-process@0.1.2"
     }
   }
 });
