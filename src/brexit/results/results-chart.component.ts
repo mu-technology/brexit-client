24225fa83985b080af29c68a5bfbc933b59f2672
Lefteris Paraskevas <eparaskevas@salesforce.com>
2016-05-06 12:17:05 +0300

    bug: remove native view encapsulation from results so it works for all browsers

--- src/brexit/results/results-chart.component.ts
+++ src/brexit/results/results-chart.component.ts
@@ -1,25 +1,11 @@
-import {Component, OnInit, ViewEncapsulation} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
 import {nvD3} from 'ng2-nvd3';
 import 'nvd3';
 declare var d3: any;
 
 @Component({
     selector: 'results-chart',
-    encapsulation: ViewEncapsulation.Native,
     template: `
-        <style>
-
-            .nvd3.nv-pie .nv-label text {
-                fill: white !important;
-                font-size: 12px;
-            }
-
-            @media only screen and (min-width: 668px) {
-                .nvd3.nv-pie .nv-label text {
-                    font-size: 18px;
-                }
-            }
-        </style>
         <div>
             <nvd3 [options]="options" [data]="data"></nvd3>    
         </div>
